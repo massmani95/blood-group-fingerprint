97939c6877629a53eb8c7b87ae47d54df14b0ef0
massmani95 <116138087+massmani95@users.noreply.github.com>
2026-02-05 21:12:12 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -109,7 +109,8 @@ def prediction():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-   app.run(host="0.0.0.0", port=10000)
+
+    app.run(host="0.0.0.0", port=10000)
 
 
 
